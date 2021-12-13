38e7ba8c786208d4a67ba7b7f7743d70b987eee0
Sergio Domínguez Gómez <91872827+SergioDominguez15@users.noreply.github.com>
2021-12-13 20:59:58 +0100

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,10 @@
+/**************** script del top nav responsive *****************/
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
